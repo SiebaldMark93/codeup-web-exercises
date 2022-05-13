62d584bf330b319790fc48db5c4412d333eefea6
Mark Siebald <Siebald.Mark93@gmail.com>
2022-05-13 16:39:48 -0500

    feat: jquery.exercise

---
+++ js/jquery_exercise.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// $(document).ready(function() {
+//
+//     alert("Dom has finished loading");
+//
+// });
+
+//Class Selectors
+$('.codeup').css("border", "1px solid red");
+
+//Element Selectors
+$('li').css('font-size', '20px');
+
+$('p')
+$('hi')
+$('li')
+
+alert($('h1').html());
+
+//Multiple Selectors
+$('p, h1, li');
